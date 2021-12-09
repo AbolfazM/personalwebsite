b40372097a8134cedb40ea4a257d04435692b0e3
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-09 15:27:57 +0330

    create Header component!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Header from './Components/Header/Header';
 
 const App = () => {
   return (
    <div className="App">
-      personal brand
+      <Header />
    </div>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+            <span>ABZA</span>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
