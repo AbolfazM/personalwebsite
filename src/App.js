346d28d07c3aefdcb22bd1f6360d8d9ab82f3662
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-09 21:03:56 +0330

    create static of Nav!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './Components/Header/Header';
+import Nav from './Components/Nav/Nav';
 
 const App = () => {
   return (
    <div className="App">
       <Header />
+      <Nav />
    </div>
   );
 }
---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => {
+    return(
+        <div>
+            <h6>I'm</h6>
+            <h6>Abolfazl Moradi</h6>
+            <p>Freelancer web developer based on ReactJs. <br />Working in Germany, Berlin.</p>
+            <button>Hire Me</button>
+        </div>
+    );
+}
+
+export default Nav;
