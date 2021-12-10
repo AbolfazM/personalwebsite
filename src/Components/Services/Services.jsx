d60d20ddbba9d4c0f1c4f8ddc062c5f137d13f11
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-10 22:56:48 +0330

    add static of services!

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,9 +1,17 @@
 import React, {Fragment} from 'react';
+import Shoppify from '../assists/shopping.png';
 
 const Services = () => {
     return(
         <Fragment>
-            <div className="Service"></div>
+            <div className="Service">
+                <div className="Service__Icons">
+                    <span>facebook</span>
+                    <span>Google</span>
+                    <img src={Shoppify} alt="shopping service" />
+                    <span>tumblr.</span>
+                </div>
+            </div>
         </Fragment>
     );
 }
