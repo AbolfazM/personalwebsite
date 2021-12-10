8570e96493d2caee623b94af7847ba96e05b4b3d
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-10 18:10:11 +0330

    responsive nav texts!

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -5,10 +5,10 @@ const Nav = () => {
     return(
         <div>
             <Me />
-            {/* <div className="Name">
+            <div className="Name">
                 <h2>I'm</h2>
                 <h1>Abolfazl Moradi</h1>
-            </div> */}
+            </div>
             {/* <p className="Description">Freelancer developer on ReactJs and professional. <br />Working in Germany, Berlin.</p>
             <button className="HireMe">Hire Me</button> */}
         </div>
