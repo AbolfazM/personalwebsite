240b2b6a7f39d6c996ac2bf204cb1d1a9cfc76d9
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-10 18:25:05 +0330

    responsive description and name!

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -9,8 +9,8 @@ const Nav = () => {
                 <h2>I'm</h2>
                 <h1>Abolfazl Moradi</h1>
             </div>
-            {/* <p className="Description">Freelancer developer on ReactJs and professional. <br />Working in Germany, Berlin.</p>
-            <button className="HireMe">Hire Me</button> */}
+            <p className="Description">Freelancer developer on ReactJs and professional. <br />Working in Germany, Berlin.</p>
+            {/* <button className="HireMe">Hire Me</button> */}
         </div>
     );
 }
