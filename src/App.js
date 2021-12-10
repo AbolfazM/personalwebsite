1aa2f61f064506d574dd3aae8de8d809a16fc874
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-10 22:25:48 +0330

    create Services component!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './Components/Header/Header';
 import Nav from './Components/Nav/Nav';
+import Services from './Components/Services/Services';
 
 const App = () => {
   return (
    <div className="App">
       <Header />
       <Nav />
+      <Services />
    </div>
   );
 }
