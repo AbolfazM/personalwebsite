1385a1f4f1a715583bffc4f60d271a2cbc4c3214
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-10 18:04:40 +0330

    responsive me.png!

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -5,12 +5,12 @@ const Nav = () => {
     return(
         <div>
             <Me />
-            <div className="Name">
+            {/* <div className="Name">
                 <h2>I'm</h2>
                 <h1>Abolfazl Moradi</h1>
-            </div>
-            <p className="Description">Freelancer developer on ReactJs and professional. <br />Working in Germany, Berlin.</p>
-            <button className="HireMe">Hire Me</button>
+            </div> */}
+            {/* <p className="Description">Freelancer developer on ReactJs and professional. <br />Working in Germany, Berlin.</p>
+            <button className="HireMe">Hire Me</button> */}
         </div>
     );
 }
