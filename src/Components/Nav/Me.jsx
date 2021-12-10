f2d25c097d4f62908a8e71e1ff730c61cda1ff9a
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-10 21:00:51 +0330

    uncomment social media icons!

--- src/Components/Nav/Me.jsx
+++ src/Components/Nav/Me.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import MyImg from '../assists/me.png';
-// import Telegram from '../assists/telegram.png';
-// import Spotify from '../assists/spotify.png';
-// import Dribbble from '../assists/dribbble.png';
+import Telegram from '../assists/telegram.png';
+import Spotify from '../assists/spotify.png';
+import Dribbble from '../assists/dribbble.png';
 
 const Me = () => {
     return(
         <div>
             <img className="Me" src={MyImg} alt="my img" />
-            {/* <div>
+            <div>
                 <img className="Social Telegram" src={Telegram} alt="telegram account" />
                 <img className="Social Spotify" src={Spotify} alt="spotify account" />
                 <img className="Social Dribbble" src={Dribbble} alt="dribbble account" />
-            </div> */}
+            </div>
         </div>
     );
 }
