19f4d7bfb0fa3d37fd5fdeec771d0be9d2aadd7e
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-10 21:19:50 +0330

    add better color to text nav!

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
             <Me />
             <div className="Name">
                 <h2>I'm</h2>
-                <h1>Abolfazl Moradi</h1>
+                <h1>A<span>b</span>olfazl M<span>o</span>radi</h1>
             </div>
             <p className="Description">Freelancer developer on ReactJs and professional. <br />Working in Germany, Berlin.</p>
             <button className="HireMe">Hire Me</button>
