063d5d4c950e6c307f1b6842d510877237455f24
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-11 16:22:29 +0330

    s

--- src/Components/Services/Cards.jsx
+++ src/Components/Services/Cards.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Cards = () => {
     return(
         <div className="Cards">
-            <div className="firstCard">1</div>
-            <div className="secondCard">2</div>
-            <div className="thirdCard">3</div>
+            <div className="firstCard"></div>
+            <div className="secondCard"></div>
+            <div className="thirdCard"></div>
         </div>
     );
 }
