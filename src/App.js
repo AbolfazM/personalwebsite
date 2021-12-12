61b8df2b1ffeab295a1de0d730c87df1f0d1ca50
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-12 22:56:57 +0330

    install Aos package and create Sample component!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useEffect } from 'react';
+import Aos from 'aos';
 import Header from './Components/Header/Header';
 import Nav from './Components/Nav/Nav';
+import Sample from './Components/SampleWorks/Sample';
 import Services from './Components/Services/Services';
 
 const App = () => {
+
+  useEffect(() => {
+    <script>
+      AOS.init();
+    </script>
+  },[]);
   return (
    <div className="App">
       <Header />
       <Nav />
       <Services />
+      <Sample />
    </div>
   );
 }
