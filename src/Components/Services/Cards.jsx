cf4326d05e817856bf038a75b983f2535c4a2818
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-12 17:26:39 +0330

    add icons!

--- src/Components/Services/Cards.jsx
+++ src/Components/Services/Cards.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Money from '../assists/money.png';
+import Laptop from '../assists/laptop.png';
+import Bulb from '../assists/light-bulb.png';
 
 const Cards = () => {
     return(
         <div className="Cards">
-            <div className="firstCard"></div>
-            <div className="secondCard"></div>
-            <div className="thirdCard"></div>
+            <div className="firstCard">
+                <img src={Money} alt="icon of firstcard" />
+            </div>
+            <div className="secondCard">
+                <img src={Laptop} alt="icon of secondcard" />
+            </div>
+            <div className="thirdCard">
+                <img src={Bulb} alt="icon of secondcard" />
+            </div>
         </div>
     );
 }
